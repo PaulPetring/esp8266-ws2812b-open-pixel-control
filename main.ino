b9b94f1f08eca2ee70e8ee6a56d5312b05a21acb
Paul Petring <PaulPetring@users.noreply.github.com>
2016-01-17 14:53:23 +0100

    added initial state, improved serial communikation

--- main.ino
+++ main.ino
@@ -2,7 +2,7 @@
 #include <WiFiUdp.h>
 #include <NeoPixelBus.h>
 
-#define pixelCount 150 // Strip has amount of pixelCount NeoPixels (5m * 30 leds/m)
+#define pixelCount 75 // Strip has amount of pixelCount NeoPixels (5m * 30 leds/m)
 #define pixelPin 2 // Strip is attached to GPIO2 on ESP-01
 unsigned int localPort = 2342;
 WiFiUDP udp;
@@ -12,14 +12,14 @@ byte packetBuffer[ MAX_PACKET_SIZE];
 
 NeoPixelBus strip = NeoPixelBus(pixelCount, pixelPin);
 
-const char *ssid = "C3D2";
-const char *password = "";
+const char *ssid = "your-ssid";
+const char *password = "your-pass";
 
 void setup()
 {
   Serial.begin ( 115200 );
   Serial.println("setup begin");
-  
+
   WiFi.begin ( ssid, password );
 
   while ( WiFi.status() != WL_CONNECTED ) {
@@ -28,12 +28,19 @@ void setup()
   }
 
   Serial.println("wifi connected");
+  
+  //fill for init reaction 
+  for (int i = 0; i <= MAX_PACKET_SIZE; i++) {
+    packetBuffer[i] = byte(128);
+  }
 
   udp.begin(localPort);
   strip.Begin();
   strip.Show();
 
   Serial.println("setup done");
+  Serial.println(WiFi.localIP());
+  setStripByData(packetBuffer, MAX_PACKET_SIZE);
 }
 
 
@@ -44,7 +51,8 @@ void loop()
     //no packet yet
   }
   else {
-    if (cb > 10) //cb is packet physical length
+    int supposedLength = byte(packetBuffer[2]) << 8 | byte(packetBuffer[3]);
+    if (cb != (supposedLength + 4)) //cb is packet physical length
     {
 
       udp.read(packetBuffer, MAX_PACKET_SIZE);
@@ -52,34 +60,27 @@ void loop()
       int prio = packetBuffer[0]; //prio 0-255
       int command = packetBuffer[1]; //we currently only support 0)
 
-      int supposedLength = byte(packetBuffer[2]) << 8 | byte(packetBuffer[3]);
+      setStripByData(packetBuffer, supposedLength);
 
-      if (cb != (supposedLength + 4)) //header
-      {
-        Serial.println("package like live - too short." + String(cb) + "!=" + String(supposedLength));
-      }
-      else
-      {
-        setStripByData(packetBuffer,supposedLength);
-      }
     }
   }
 }
 
 void setStripByData(byte* data, int sl)
 {
-  Serial.println(sl);
+  //Serial.println(sl);
   int led = 0;
   for (int i = 4; i <= sl; i = i + 3) {
-   /* if (led < 3) //love for leds
-    {
-      Serial.print(data[i]); Serial.print(" ");
-      Serial.print(data[i + 1]); Serial.print(" ");
-      Serial.print(data[i + 2]); Serial.print(" ");
-    }
-   */
+    /* if (led < 3) //love for leds
+     {
+       Serial.print(data[i]); Serial.print(" ");
+       Serial.print(data[i + 1]); Serial.print(" ");
+       Serial.print(data[i + 2]); Serial.print(" ");
+     }
+    */
     strip.SetPixelColor(led, RgbColor(data[i], data[i + 1], data[i + 2]));
     led++;
   }
   strip.Show();
+  Serial.print ( "." );
 }
